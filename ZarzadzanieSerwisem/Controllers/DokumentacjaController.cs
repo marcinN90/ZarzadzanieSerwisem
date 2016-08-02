693880f8465400eaadd703ce493e6ef921017cd9
marcinN90 <90.marcin.nowak@gmail.com>
2016-08-03 00:36:07 +0200

    Dokumentacja Serwisowa

---
+++ ZarzadzanieSerwisem/Controllers/DokumentacjaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZarzadzanieSerwisem.Controllers
+{
+    public class DokumentacjaController : Controller
+    {
+        // GET: Dokumentacja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
